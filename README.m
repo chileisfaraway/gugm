695b5c11eda44e9ec584d0bb7d0508ee4d7e0bfc
Ben Holloway <ben@holloway.gb.com>
2013-08-16 16:23:43 +0100

    additions...

---
+++ README.m
@@ -0,0 +1,17 @@
+Hello,
+
+<b>Get up Get Moving Project.</b>
+GCSE I.T.
+
+I've attempted to tidy the code. Most of the pages use ?php? to import the
+header and the footer file which are in <html> files. There is some javascript
+and some jQuery for animations but any processing is done in ?php?
+
+The quiz section is largely custom made and all of the processsing is done
+in ?php? and all of the information is stored in text files.
+
+This is a very messy project. It's purpose made and all of the content is 
+static which makes it kind of pointless putting it on github but I need
+version control.
+
+Ben xx
